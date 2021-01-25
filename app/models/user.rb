56d2f727d47bdabfe4f8f69d887981eba36143d3
geetha <geethatulasi007@gmail.com>
2021-01-25 13:45:32 +0530

    before deploying into heroku

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :validatable
+  has_many :messages
+  has_many :comments
+end
